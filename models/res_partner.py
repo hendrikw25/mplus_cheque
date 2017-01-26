b613bb1953214fd4403109b5e19c12cc6132752e
hendrikw25 <hendrikwidjaja25@gmail.com>
2017-01-26 18:35:27 +0700

    Direct print cheque book on Windows platform

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 from openerp.osv import fields, osv
+from openerp import tools
+
+import logging
+_logger = logging.getLogger(__name__)
 
 class res_partner(osv.osv):
     _inherit = 'res.partner'
@@ -14,3 +18,21 @@ class res_partner(osv.osv):
     _defaults = {
         'number_of_print': 1,
     }
+    
+    def direct_print_cheque_book(self, cr, uid, ids, context=None):
+        try:
+            import win32api
+            import tempfile
+            import os
+            pdf = self.pool.get('report').get_pdf(cr, uid, ids, 'mplus_cheque.report_cheque_book', context=context)
+            f = tempfile.NamedTemporaryFile(suffix='.pdf', delete=False)
+            f.write(pdf)
+            f.close()
+            win32api.ShellExecute(0, "print", f.name, None,  ".",  0)
+            #os.unlink(f.name)  #TO DO: wait print process to finish before printing
+        except Exception, ex:
+            _logger.info('Could not print: %s', tools.ustr(ex))
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload'
+        }
